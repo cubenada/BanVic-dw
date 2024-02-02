230e80fa6361d47312dd518e1b295efaf004d88a
Lorenzo Garibotti Sfredo <cubenada@gmail.com>
2024-02-02 07:48:17 +0000

    ajustes finais 1

--- models/staging/stg_contas.sql
+++ models/staging/stg_contas.sql
@@ -1,15 +1,15 @@
 with
     source_contas as (
         select
-            cast(num_conta as int) as num_conta,
-            cast(cod_cliente as int) as cliente_id,
-            cast(cod_agencia as int) as agencia_id,
-            cast(cod_colaborador as int) as colaborador_id,
-            contas.tipo_conta as tipo_conta,
-            DATE(data_abertura) as data_abertura,
-            cast(saldo_total as numeric) as saldo_total,
-            cast(saldo_disponivel as numeric) as saldo_disponivel,
-            DATE(data_ultimo_lancamento) as ultimo_lancamento
+            cast(num_conta as int) as num_conta
+            , cast(cod_cliente as int) as cliente_id
+            , cast(cod_agencia as int) as agencia_id
+            , cast(cod_colaborador as int) as colaborador_id
+            , contas.tipo_conta as tipo_conta
+            , DATE(data_abertura) as data_abertura
+            , cast(saldo_total as numeric) as saldo_total
+            , cast(saldo_disponivel as numeric) as saldo_disponivel
+            , DATE(data_ultimo_lancamento) as ultimo_lancamento
         from {{ source('lgs_source', 'contas') }}
     )
 select *
--- models/staging/stg_propostas_credito.sql
+++ models/staging/stg_propostas_credito.sql
@@ -1,18 +1,18 @@
 with
     source_propostas_credito as (
         select
-            cast(cod_proposta as int) as proposta_id,
-            cast(cod_cliente as int) as cliente_id,
-            cast(cod_colaborador as int) as colaborador_id,
-            DATE(data_entrada_proposta) as data_proposta,
-            cast(taxa_juros_mensal as numeric) as juros_mensal,
-            cast(valor_proposta as numeric) as valor_proposta,
-            cast(valor_financiamento as numeric) as valor_financiamento,
-            cast(valor_entrada as numeric) as valor_entrada,
-            cast(valor_prestacao as numeric) as valor_prestacao,
-            cast(quantidade_parcelas as int) as qntd_parcelas,
-            cast(carencia as int) as carencia,
-            propostas_credito.status_proposta as status
+            cast(cod_proposta as int) as proposta_id
+            , cast(cod_cliente as int) as cliente_id
+            , cast(cod_colaborador as int) as colaborador_id
+            , DATE(data_entrada_proposta) as data_proposta
+            , cast(taxa_juros_mensal as numeric) as juros_mensal
+            , cast(valor_proposta as numeric) as valor_proposta
+            , cast(valor_financiamento as numeric) as valor_financiamento
+            , cast(valor_entrada as numeric) as valor_entrada
+            , cast(valor_prestacao as numeric) as valor_prestacao
+            , cast(quantidade_parcelas as int) as qntd_parcelas
+            , cast(carencia as int) as carencia
+            , propostas_credito.status_proposta as status
         from {{ source('lgs_source', 'propostas_credito') }}
     )
 select *
